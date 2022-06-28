92d39715313e77cb7d608a498e76ea697935997d
Jamie Collins <james.r.collins@aya.yale.edu>
2022-06-27 21:12:59 -0700

    Making progress with coordinate match-making

--- ConstrainCO2Flux.R
+++ ConstrainCO2Flux.R
@@ -9,6 +9,8 @@
 library(raster) # assumes you have some version of GDAL up and running, and 
                 # will force you to load several dependencies, including terra 
 library(rgdal)
+library(sp)
+library(data.table)
 
 # library(ncdf4) # not needed anymore
 
@@ -34,19 +36,60 @@ fseq_bottom_100yr.raw <- read.csv("data/derived/benthic_seqfractions/fseq_bottom
                                  header = FALSE)
 fseq_bottom_depth_m.raw <- read.csv("data/derived/benthic_seqfractions/bottom_depth_m.csv",
                                  header = FALSE)
-fseq_bottom_lat_degNr.raw <- read.csv("data/derived/benthic_seqfractions/lat_degN.csv",
+fseq_bottom_lat_degN.raw <- read.csv("data/derived/benthic_seqfractions/lat_degN.csv",
                                  header = FALSE)
 fseq_bottom_long_degE.raw <- read.csv("data/derived/benthic_seqfractions/long_degE.csv",
                                  header = FALSE)
 
-# load Sala et al. pCO2 data, as GeoTIFF
-# with some help from https://datacarpentry.org/r-raster-vector-geospatial/01-raster-structure/
+# load Sala et al. pCO2 data, as GeoTIFF, then convert to data frames
+# with some help from https://datacarpentry.org/r-raster-vector-geospatial/01-raster-structure/ and
+# https://www.neonscience.org/resources/learning-hub/tutorials/raster-data-r
 
 Sala_bottomtrawl_Ia.raw <- 
   raster("data/raw/sala_et_al_2021/bottom_trawling_Ia.tif")
+Sala_bottomtrawl_Ia.df <- as.data.frame(Sala_bottomtrawl_Ia.raw, xy = TRUE)
 
 Sala_carbon_ranking.raw <- 
   raster("data/raw/sala_et_al_2021/carbon_ranking.tif")
+Sala_carbon_ranking.df <- as.data.frame(Sala_carbon_ranking.raw, xy = TRUE)
 
 Sala_CO2_efflux.raw <- 
   raster("data/raw/sala_et_al_2021/co2_efflux.tif")
+# Sala_CO2_efflux.df <- as.data.frame(Sala_CO2_efflux.raw, xy = TRUE) 
+# # note that creating a data frame from this last object will require a lot of memory ... I had to up 
+# # the R_MAX_VSIZE variable in .Renviron according to the directions here:
+# # https://stackoverflow.com/questions/51248293/error-vector-memory-exhausted-limit-reached-r-3-5-0-macos
+
+# now, can find most appropriate (nearest) sequestration fraction for each point in the CO2 flux dataset
+
+# create data frames of the coordinates of the points in the two different datasets
+
+# CO2 flux data
+Sala_CO2_efflux.coords <- xyFromCell(Sala_CO2_efflux.raw,c(1:length(Sala_CO2_efflux.raw))) # easy, can just use the xyFromCell function in raster package
+
+# sequestration fractions
+Siegel_fseq.coords <- data.frame(as.numeric(rep(fseq_bottom_long_degE.raw[1,],91)),rep(fseq_bottom_lat_degN.raw[,1],180))
+colnames(Siegel_fseq.coords) <- c("x","y")
+# convert to eastings & westings rather than just eastings, for compatibility 
+Siegel_fseq.coords$x[Siegel_fseq.coords$x>180] <- Siegel_fseq.coords$x[Siegel_fseq.coords$x>180]-360
+
+# now have (more or less) apples to oranges; find best match for each point in the CO2 flux dataset
+
+# convert to data tables
+Sala_CO2_efflux.coords.dt <- data.table(Sala_CO2_efflux.coords)
+Siegel_fseq.coords.dt <- data.table(Siegel_fseq.coords)
+
+# define function to find nearest match 
+
+dist <- function(a, b){
+  dt <- data.table((Siegel_fseq.coords.dt$x-a)^2+(Siegel_fseq.coords.dt$y-b)^2)
+  return(which.min(dt$V1))}
+
+coord.matches <- Sala_CO2_efflux.coords.dt[, j = list(Closest =  dist(x, y)), by = 1:nrow(Sala_CO2_efflux.coords.dt)]
+
+dist.alt <- function(a, b){
+  dt <- data.table((Sala_CO2_efflux.coords.dt$x-a)^2+(Sala_CO2_efflux.coords.dt$y-b)^2)
+  return(which.min(dt$V1))}
+
+coord.matches.alt <- Siegel_fseq.coords.dt[, j = list(Closest =  dist(x, y)), by = 1:nrow(Siegel_fseq.coords.dt)]
+
