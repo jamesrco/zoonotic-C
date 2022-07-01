4e334a22dd0b3f4e69c530d48658b7c84d137303
Jamie Collins <james.r.collins@aya.yale.edu>
2022-07-01 13:04:56 -0700

    Another provisioning script to set up a swap file

---
+++ aws_provisioning/03_ubuntu_swapmake.sh
@@ -0,0 +1,25 @@
+# 03_ubuntu_swapmake.sh
+
+# provisioning script for an AWS EC2 instance running Ubuntu
+# this script creates a swapfile to dedicate disk space as memory
+# useful to avert killing of R sessions due to out of memory errors
+
+# created 1 Jul 2022 by Jamie Collins, jcollins@edf.org; tested under Ubuntu 22.04
+# with help from https://www.digitalocean.com/community/tutorials/how-to-add-swap-space-on-ubuntu-20-04
+
+# assumes you have at least as much storage on your image as dedicated to a swap
+# latest test: allocates 75 GB of 100 GB total storage (7/1/22)
+
+sudo fallocate -l 75G /swapfile
+
+sudo chmod 600 /swapfile
+
+sudo mkswap /swapfile
+
+sudo swapon /swapfile
+
+# to make sure it's enabled and working:
+
+sudo swapon --show
+
+free -h
