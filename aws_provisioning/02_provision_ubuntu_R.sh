a22570a439dc98c8e4ab6fc61fa5f1357acbb1ab
Jamie Collins <james.r.collins@aya.yale.edu>
2022-07-02 19:37:29 -0700

    Change order of provisioning

--- aws_provisioning/02_provision_ubuntu_R.sh
+++ aws_provisioning/02_provision_ubuntu_R.sh
@@ -38,13 +38,13 @@ sudo DEBIAN_FRONTEND=noninteractive apt update
 
 echo "Y" | sudo DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends r-base
 
+# install OpenMP (so data.table can take advantage of some parallelization)
+
+echo "Y" | sudo DEBIAN_FRONTEND=noninteractive apt install -y libomp-dev
+
 # install necessary packages for working with some spatial data, including data.table
 # some help from https://geocompr.github.io/post/2020/installing-r-spatial-ubuntu/
 
 echo "Y" | sudo DEBIAN_FRONTEND=noninteractive apt install -y libudunits2-dev libgdal-dev libgeos-dev libproj-dev libfontconfig1-dev
 
 echo "Y" | sudo DEBIAN_FRONTEND=noninteractive apt install -y r-base-dev r-cran-sf r-cran-raster r-cran-rjava r-cran-data.table
-
-# install OpenMP (so data.table can take advantage of some parallelization)
-
-echo "Y" | sudo DEBIAN_FRONTEND=noninteractive apt install -y libomp-dev
