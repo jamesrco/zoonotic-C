b90fecd6495d71d72a1b138031a689f86f6ea8ef
Jamie Collins <james.r.collins@aya.yale.edu>
2022-07-07 14:40:51 -0700

    Adds cumulative atmos. emissions estimate over 100 years

--- 03_ConstrainCO2Flux_adjFlux.R
+++ 03_ConstrainCO2Flux_adjFlux.R
@@ -6,7 +6,8 @@
 # Author: Jamie Collins, jcollins@edf.org
 
 # This third script performs the actual adjustment of the Sala et al. benthic
-# CO2 flux data
+# CO2 flux data and includes a time-integrated calculation for total emissions
+# to the atmosphere over a 100 year time period
 
 # *** Assumes user has already run 01_ConstrainCO2Flux_IO.R (in current session)
 # and that the object "coord.matches.RData" generated using
@@ -39,12 +40,31 @@ library(parallel) # part of base; doesn't need to be installed
 
 load("data/derived/output/coord.matches.RData")
 
+# load the summed emissions fractions for 1 to 100 y (previously generated using 
+# gen_fracs_to_constrain_trawlCO2.m)
+
+sum_of_fseq_bottom_1to100years.raw <- read.csv(file = "data/derived/benthic_seqfractions/sum_of_fseq_bottom_1to100years.csv",
+                                               header = FALSE)
+
+# load the year-by-year predicted global CO2 sediment remineralization rates from Sala et al.
+# (this is a static .csv version of the object "results," generated from the simple
+# model beginning on line 119 in
+# https://github.com/emlab-ucsb/ocean-conservation-priorities/blob/master/ancillary_analyses/timing_of_trawling_impacts.Rmd)
+
+Sala_et_al_trawlTiming_results.raw <- read.csv(file = "data/derived/sala_et_al_2021_model/Sala_et_al_trawlTiming_results.csv",
+                                           header = TRUE) 
+
+colnames(Sala_et_al_trawlTiming_results.raw)[1] <- c("Year")
+
 # define some efflux fractions for each of the timescales (should be 1 - the
 # sequestration fraction) and some functions to perform the calculations
 
 effluxFrac.1yr <- 1-as.numeric(unlist(fseq_bottom_1yr.raw))
+effluxFrac.5yr <- 1-as.numeric(unlist(fseq_bottom_5yr.raw))
+effluxFrac.10yr <- 1-as.numeric(unlist(fseq_bottom_10yr.raw))
 effluxFrac.25yr <- 1-as.numeric(unlist(fseq_bottom_25yr.raw))
 effluxFrac.50yr <- 1-as.numeric(unlist(fseq_bottom_50yr.raw))
+effluxFrac.75yr <- 1-as.numeric(unlist(fseq_bottom_75yr.raw))
 effluxFrac.100yr <- 1-as.numeric(unlist(fseq_bottom_100yr.raw))
 effluxFrac.1000yr <- 1-as.numeric(unlist(fseq_bottom_1000yr.raw))
 
@@ -56,6 +76,16 @@ length(ind.nonZeroCO2)/length(Sala_CO2_efflux.df$co2_efflux) # values that aren'
 
 constrainFlux.1yr <- function(a){
   adjFlux <- Sala_CO2_efflux.df$co2_efflux[a]*effluxFrac.1yr[coord.matches$Closest[a]]
+  return(adjFlux)
+}
+
+constrainFlux.5yr <- function(a){
+  adjFlux <- Sala_CO2_efflux.df$co2_efflux[a]*effluxFrac.5yr[coord.matches$Closest[a]]
+  return(adjFlux)
+}
+
+constrainFlux.10yr <- function(a){
+  adjFlux <- Sala_CO2_efflux.df$co2_efflux[a]*effluxFrac.10yr[coord.matches$Closest[a]]
   return(adjFlux)
 }
 
@@ -72,6 +102,11 @@ constrainFlux.25yr <- function(a){
 
 constrainFlux.50yr <- function(a){
   adjFlux <- Sala_CO2_efflux.df$co2_efflux[a]*effluxFrac.50yr[coord.matches$Closest[a]]
+  return(adjFlux)
+}
+
+constrainFlux.75yr <- function(a){
+  adjFlux <- Sala_CO2_efflux.df$co2_efflux[a]*effluxFrac.75yr[coord.matches$Closest[a]]
   return(adjFlux)
 }
 
@@ -155,6 +190,25 @@ adjCO2efflux.1000yr$adjCO2_efflux.1000yr[ind.nonZeroCO2] <- adjCO2efflux.1000yr_
 save(adjCO2efflux.1000yr, file = "data/derived/output/adjCO2efflux.1000yr.RData")
 gc()
 
+# 5, 10, 75 year scenarios; for prediction curve
+time0 <- Sys.time()
+adjCO2efflux.5yr_nonZero <- unlist(lapply(ind.nonZeroCO2, constrainFlux.5yr))
+time1 <- Sys.time()
+print(time1 - time0)
+sum(adjCO2efflux.5yr_nonZero, na.rm=T)*SalaModel_cell_area*(1/10^9)
+
+time0 <- Sys.time()
+adjCO2efflux.10yr_nonZero <- unlist(lapply(ind.nonZeroCO2, constrainFlux.10yr))
+time1 <- Sys.time()
+print(time1 - time0)
+sum(adjCO2efflux.10yr_nonZero, na.rm=T)*SalaModel_cell_area*(1/10^9)
+
+time0 <- Sys.time()
+adjCO2efflux.75yr_nonZero <- unlist(lapply(ind.nonZeroCO2, constrainFlux.75yr))
+time1 <- Sys.time()
+print(time1 - time0)
+sum(adjCO2efflux.75yr_nonZero, na.rm=T)*SalaModel_cell_area*(1/10^9)
+
 # get some basic statistics; with unadjusted Sala et al data for comparison
 
 sum(adjCO2efflux.1yr, na.rm=T)
@@ -199,20 +253,95 @@ sum(Sala_CO2_efflux.df$co2_efflux*SalaModel_cell_area, na.rm=T)*(1/10^9) # in Pg
 # now, save these values to file
 
 adjCO2efflux_PgCO2_yr <- c(sum(adjCO2efflux.1yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
-  sum(adjCO2efflux.25yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
-  sum(adjCO2efflux.50yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
-  sum(adjCO2efflux.100yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
-  sum(adjCO2efflux.1000yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
-    sum(Sala_CO2_efflux.df$co2_efflux*SalaModel_cell_area, na.rm=T)*(1/10^9))
+                           sum(adjCO2efflux.5yr_nonZero, na.rm=T)*SalaModel_cell_area*(1/10^9),
+                           sum(adjCO2efflux.10yr_nonZero, na.rm=T)*SalaModel_cell_area*(1/10^9),
+                           sum(adjCO2efflux.25yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
+                           sum(adjCO2efflux.50yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
+                           sum(adjCO2efflux.75yr_nonZero, na.rm=T)*SalaModel_cell_area*(1/10^9),
+                           sum(adjCO2efflux.100yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
+                           sum(adjCO2efflux.1000yr*SalaModel_cell_area, na.rm=T)*(1/10^9),
+                           sum(Sala_CO2_efflux.df$co2_efflux*SalaModel_cell_area, na.rm=T)*(1/10^9))
 
 names(adjCO2efflux_PgCO2_yr) <- c("adjCO2efflux.1yr_PgCO2_per_y",
+                                  "adjCO2efflux.5yr_PgCO2_per_y",
+                                  "adjCO2efflux.10yr_PgCO2_per_y",
                                   "adjCO2efflux.25yr_PgCO2_per_y",
                                   "adjCO2efflux.50yr_PgCO2_per_y",
+                                  "adjCO2efflux.75yr_PgCO2_per_y",
                                   "adjCO2efflux.100yr_PgCO2_per_y",
                                   "adjCO2efflux.1000yr_PgCO2_per_y",
                                   "CO2efflux_PgCO2_per_y.unadjusted")
 
-write.csv(adjCO2efflux_PgCO2_yr, file = "data/derived/output/adjCO2efflux_PgCO2_yr.csv")
+write.csv(adjCO2efflux_PgCO2_yr, file = "data/derived/output/adjCO2efflux_PgCO2_yr.csv",
+          row.names = TRUE)
+
+# now we can make a time-integrated estimate of total emissions to the
+# atmosphere over a 100 year time period, using the same trawling scenario
+# as in the original Sala et al paper
+
+# first, need to make a prediction curve from the model results we have
+
+adjCO2efflux_PgCO2_yr.for_prediction <- as.data.frame(matrix(data = NA, 
+                                                             nrow = 7,
+                                                             ncol = 3))
+colnames(adjCO2efflux_PgCO2_yr.for_prediction) = c("Year",
+             "PgCO2_per_year_to_atmos",
+             "Sum_emitFrac")
+
+adjCO2efflux_PgCO2_yr.for_prediction[,2] <- adjCO2efflux_PgCO2_yr[1:7]
+adjCO2efflux_PgCO2_yr.for_prediction[,1] <- c(1,5,10,25,50,75,100)
+adjCO2efflux_PgCO2_yr.for_prediction[,3] <- sum_of_fseq_bottom_1to100years.raw[sum_of_fseq_bottom_1to100years.raw[,1] %in% adjCO2efflux_PgCO2_yr.for_prediction[,1],2]
+
+model <- lm(PgCO2_per_year_to_atmos ~ poly(Sum_emitFrac,2, raw = TRUE), data = adjCO2efflux_PgCO2_yr.for_prediction)
+
+# > summary(model)
+# 
+# Call:
+#   lm(formula = PgCO2_per_year_to_atmos ~ poly(Sum_emitFrac, 2, 
+#                                               raw = TRUE), data = adjCO2efflux_PgCO2_yr.for_prediction)
+# 
+# Residuals:
+#   1          2          3          4          5          6          7 
+# -2.372e-04  1.114e-04  6.728e-05  3.818e-04  7.092e-06 -7.310e-04  4.006e-04 
+# 
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)                        -2.133e-03  5.112e-04  -4.172  0.01400 *  
+#   poly(Sum_emitFrac, 2, raw = TRUE)1  6.214e-05  2.251e-06  27.603 1.02e-05 ***
+#   poly(Sum_emitFrac, 2, raw = TRUE)2 -1.662e-08  1.935e-09  -8.590  0.00101 ** 
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+# 
+# Residual standard error: 0.000478 on 4 degrees of freedom
+# Multiple R-squared:  0.9994,	Adjusted R-squared:  0.9991 
+# F-statistic:  3347 on 2 and 4 DF,  p-value: 3.567e-07
+
+# make some predictions for 1 to 100 y using the model we just fit
+
+predicted.PgCO2_per_year_to_atmos <- as.data.frame(matrix(data = NA, 
+                                                          nrow = 100,
+                                                          ncol = 2))
+colnames(predicted.PgCO2_per_year_to_atmos) = c("Year",
+                                                "PgCO2_per_year_to_atmos.predicted")
+predicted.PgCO2_per_year_to_atmos[,1] <- c(1:100)
+predicted.PgCO2_per_year_to_atmos[,2] <- predict(model2,newdata=data.frame(Sum_emitFrac = sum_of_fseq_bottom_1to100years.raw[,2]))
+
+# now, can make a time-integrated calculation over 100 years
+
+# assumes:
+# 1. annual CO2 flux from sediments follows the trend posited by Sala et al. 2021,
+# contained in the object "results" that is generated beginning on line 119 in
+# https://github.com/emlab-ucsb/ocean-conservation-priorities/blob/master/ancillary_analyses/timing_of_trawling_impacts.Rmd
+# *** this is where the assertion in the Sala et al. paper that sediment emissions
+# in the first year are 1.47 Pg CO2, declining after 10 y to 0.58 Pg CO2, comes from 
+# 2. the fraction of the sediment emissions in year n that will reach the atmosphere
+# after 100 y is described by the benthic emissions fraction for the nth year 
+# based on Siegel et al. 2021
+
+sum(rev(predicted.PgCO2_per_year_to_atmos[,2])*(Sala_et_al_trawlTiming_results.raw$C_remin[1:100]/
+                                                  Sala_et_al_trawlTiming_results.raw$C_remin[1]))
+
+# after 100 y of continuous trawling, a cumulative 1.27 Pg CO2 will have reached the atmosphere 
 
 # # send email when done ... assumes SSMTP has been installed and config file and text file for the email are in right place, etc.
 # 
